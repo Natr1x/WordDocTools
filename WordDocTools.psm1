a2e52a18875e8e5e7ac6667c39a11974eae9b99c
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2023-07-05 11:37:47 +0200

    Added functions to work with paragraphs and tables

--- WordDocTools.psm1
+++ WordDocTools.psm1
@@ -53,3 +53,85 @@ function Get-XPathArgs {
     return $selArg
 }
 Export-ModuleMember -Function Get-XPathArgs
+
+<#
+    .Synopsis
+    Extract and join the text from the runs in a '<w:p>' node.
+
+    .Description
+    Extract and join the text from the runs in a '<w:p>' node.
+
+    .Parameter Node
+    The xml node to extract. Should be a '<w:p>' node.
+
+    .Example
+    $xpathargs = Get-XPathArgs -Path '.\MyExample.docx'
+    $paragraphs = Select-Xml -XPath '//w:p' @xpathargs |% Node
+    Get-ParagraphText -Node $paragraphs
+#>
+function Get-ParagraphText {
+    param(
+        [Parameter(
+            Position = 0,
+            ValueFromPipeLine = $True,
+            ValueFromPipeLineByPropertyName = $True)]
+        [System.Xml.XmlNode[]]$Node
+    )
+    process {
+        Write-Verbose "Get-ParagraphText: Processing Paragraph"
+        foreach ($p in $Node) {
+            -join ($p.r.t |% {
+                if ($_ -is [string]) {
+                    $_
+                } elseif ($_.'#text') {
+                    $_.'#text'
+                }
+            })
+        }
+    }
+}
+Export-ModuleMember -Function Get-ParagraphText
+
+<#
+    .Synopsis
+    Extract the text from the cells in a '<w:tbl>' node.
+
+    .Description
+    Extract the text from the cells in a '<w:tbl>' node. Use something like
+    'Select-Xml -XPath "//w:tbl" @worddocargs |% Node' to acquire the nodes.
+
+    .Parameter Node
+    The xml node to extract. Should be a '<w:tbl>' node.
+
+    .Parameter CellDelimiter
+    Separator to delimit cells with inside a row.
+
+    .Example
+    $xpathargs = Get-XPathArgs -Path '.\MyExample.docx'
+    $doctables = Select-Xml -XPath '//w:tbl' @xpathargs |% Node
+    Get-TableCells -Node ($doctables[0])
+#>
+function Get-TableCells {
+    param(
+        [Parameter(
+            Position = 0,
+            ValueFromPipeLine = $True,
+            ValueFromPipeLineByPropertyName = $True)]
+        [System.Xml.XmlNode[]]$Node,
+        [Parameter(Mandatory = $False)]
+        [string]$CellDelimiter = " | "
+    )
+    process {
+        Write-Verbose "Get-TableCells: Processing TableNode"
+        foreach ($t in $Node) {
+            foreach ($r in $t.tr) {
+                $cols = @()
+                foreach ($c in $r.tc) {
+                    $cols += ,(Get-ParagraphText $c.p)
+                }
+                $cols -join "$CellDelimiter"
+            }
+        }
+    }
+}
+Export-ModuleMember -Function Get-TableCells
